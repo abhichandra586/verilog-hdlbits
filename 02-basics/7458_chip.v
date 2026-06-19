5d6f3a0e7987ea9bd6cac8827b92f90eeae832c2
B. Abhi Chandra <24l31a0436@vignaniit.edu.in>
2026-06-19 21:37:49 +0530

    Solve HDLBits: 7458 Chip

---
+++ 02-basics/7458_chip.v
@@ -0,0 +1,22 @@
+// HDLBits: 7458 Chip
+// https://hdlbits.01xz.net/wiki/7458
+
+module top_module ( 
+    input p1a, p1b, p1c, p1d, p1e, p1f,
+    output p1y,
+    input p2a, p2b, p2c, p2d,
+    output p2y
+);
+    
+    wire a,b,c,d;
+    
+    assign a = p1a&p1b&p1c;
+    assign b = p2a&p2b;
+    assign c = p2c&p2d;
+    assign d = p1d&p1e&p1f;
+    
+    assign p1y = a|d;
+    assign p2y = b|c;
+
+
+endmodule
