15defdeebfb10ffdea02f1bfe0da793f74c237ba
B. Abhi Chandra <24l31a0436@vignaniit.edu.in>
2026-06-19 21:06:52 +0530

    Solve HDLBits: Four Wires

---
+++ 02-basics/four_wires.v
@@ -0,0 +1,14 @@
+// HDLBits: Four Wires
+// https://hdlbits.01xz.net/wiki/Wire4
+
+module top_module( 
+    input a,b,c,
+    output w,x,y,z
+);
+    
+    assign w=a;
+    assign x=b;
+    assign y=b;
+    assign z=c;
+
+endmodule
