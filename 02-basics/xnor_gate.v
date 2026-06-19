0508913b245539f6318f67afa0d888ab8c0b7089
B. Abhi Chandra <24l31a0436@vignaniit.edu.in>
2026-06-19 21:19:09 +0530

    Solve HDLBits: XNOR Gate

---
+++ 02-basics/xnor_gate.v
@@ -0,0 +1,12 @@
+// HDLBits: XNOR Gate
+// https://hdlbits.01xz.net/wiki/Xnorgate
+
+module top_module( 
+    input a, 
+    input b, 
+    output out
+);
+    
+    assign out = ~(a^b);
+
+endmodule
