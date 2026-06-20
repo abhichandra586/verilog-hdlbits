35c91195cef53f79b16f2d0f1d9f4612b5543a3f
B. Abhi Chandra <24l31a0436@vignaniit.edu.in>
2026-06-21 00:05:57 +0530

    Solve HDLBits: Connecting ports by position

---
+++ 04-modules-hierarchy/module_pos.v
@@ -0,0 +1,15 @@
+// HDLBits: Connecting ports by position
+// https://hdlbits.01xz.net/wiki/Module_pos
+
+module top_module ( 
+    input a, 
+    input b, 
+    input c,
+    input d,
+    output out1,
+    output out2
+);
+    
+    mod_a abc(out1,out2,a,b,c,d);
+
+endmodule
