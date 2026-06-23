f2b5207e2d9db64bf00b8911433bda063b96dba7
B. Abhi Chandra <24l31a0436@vignaniit.edu.in>
2026-06-23 21:57:38 +0530

    Solve HDLBits: Always If

---
+++ 05-procedures/always_if.v
@@ -0,0 +1,21 @@
+// HDLBits: If statement (Always If)
+// http://hdlbits.01xz.net/wiki/Always_if
+
+module top_module(
+    input a,
+    input b,
+    input sel_b1,
+    input sel_b2,
+    output wire out_assign,
+    output reg out_always
+); 
+    
+    assign out_assign = (sel_b1 && sel_b2) ? b : a;
+    
+    always @(*) begin
+        if(sel_b1 && sel_b2)
+            out_always = b;
+        else out_always = a;
+    end
+    
+endmodule
