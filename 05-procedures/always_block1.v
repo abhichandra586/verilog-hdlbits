c1100e23a55e1906138d6f9176b6eed8cb00cfab
B. Abhi Chandra <24l31a0436@vignaniit.edu.in>
2026-06-23 21:51:21 +0530

    Solve HDLBits: Always Block 1

---
+++ 05-procedures/always_block1.v
@@ -0,0 +1,16 @@
+// HDLBits: Always Block 1
+// https://hdlbits.01xz.net/wiki/Alwaysblock1
+
+module top_module(
+    input a, 
+    input b,
+    output wire out_assign,
+    output reg out_alwaysblock
+);
+    
+    assign out_assign = a & b;
+    always @(*) begin
+        out_alwaysblock = a & b;
+    end
+
+endmodule
