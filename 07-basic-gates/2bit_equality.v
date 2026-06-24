6bca725182d5ed92d55c10bb4ab11355b78f4a52
B. Abhi Chandra <24l31a0436@vignaniit.edu.in>
2026-06-25 02:08:04 +0530

    Solve HDLBits: Two bit equality

---
+++ 07-basic-gates/2bit_equality.v
@@ -0,0 +1,12 @@
+// HDLBits: Two Bit Equality
+// https://hdlbits.01xz.net/wiki/Mt2015_eq2
+
+module top_module (
+    input [1:0] A,
+    input [1:0] B,
+    output z 
+); 
+    
+    assign z = (A==B) ? 1 : 0;
+
+endmodule
