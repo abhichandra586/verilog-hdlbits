accef11d62fcd213a6f93059f2957f7534127a6e
B. Abhi Chandra <24l31a0436@vignaniit.edu.in>
2026-06-24 22:31:36 +0530

    Solve HDLBits: Reduction Operators

---
+++ 06-more-verilog-features/reduction_operators.v
@@ -0,0 +1,10 @@
+// HDLBits: Reduction Operators
+// https://hdlbits.01xz.net/wiki/Reduction
+
+module top_module (
+    input [7:0] in,
+    output parity);
+    
+    assign parity = ^in;
+
+endmodule
