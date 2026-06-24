9abc87fda135c019bfb3557177445a5db158f7cf
B. Abhi Chandra <24l31a0436@vignaniit.edu.in>
2026-06-25 02:12:59 +0530

    Solve HDLBits: Simple Circuit B

---
+++ 07-basic-gates/simple_circuitB.v
@@ -0,0 +1,12 @@
+// HDLBits: Simple Circuit B
+// https://hdlbits.01xz.net/wiki/Mt2015_q4b
+
+module top_module ( 
+    input x,
+    input y,
+    output z
+);
+    
+    assign z = ~(x^y);
+
+endmodule
