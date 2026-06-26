26fc893c1d61ee3563f401d5c42e6c970cdaf764
B. Abhi Chandra <24l31a0436@vignaniit.edu.in>
2026-06-26 14:14:42 +0530

    Solve HDLBits: 3 bit population count

---
+++ 07-basic-gates/pop_count3.v
@@ -0,0 +1,11 @@
+// 3 bit population count
+// https://hdlbits.01xz.net/wiki/Popcount3
+
+module top_module( 
+    input [2:0] in,
+    output [1:0] out
+);
+    
+    assign out = in[0] + in[1] + in[2];
+
+endmodule
