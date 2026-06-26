34b95bb1b3d153a1e6059a6988b7d8c35f54d019
B. Abhi Chandra <24l31a0436@vignaniit.edu.in>
2026-06-26 14:13:50 +0530

    Solve HDLBits: Thermostat

---
+++ 07-basic-gates/thermostat.v
@@ -0,0 +1,18 @@
+// HDLBits: Thermostat
+// https://hdlbits.01xz.net/wiki/Thermostat
+
+module top_module (
+    input too_cold,
+    input too_hot,
+    input mode,
+    input fan_on,
+    output heater,
+    output aircon,
+    output fan
+); 
+    
+    assign heater = too_cold? mode:0;
+    assign aircon = too_hot? ~mode:0;
+    assign fan = (fan_on | heater | aircon)? 1:0;
+
+endmodule
