eb928235335e8efacd4f6f716a7f23bba6ceb79d
B. Abhi Chandra <24l31a0436@vignaniit.edu.in>
2026-06-28 13:02:06 +0530

    Solve HDLBits: Full Adder

---
+++ 09-arithmetic-circuits/full_adder.v
@@ -0,0 +1,12 @@
+// HDLBits: Full Adder
+// http://hdlbits.01xz.net/wiki/Fadd
+
+module top_module( 
+    input a, b, cin,
+    output cout, sum 
+);
+    
+    assign sum = a^b^cin;
+    assign cout = (a&b) | (b&cin) | (cin&a);
+
+endmodule
